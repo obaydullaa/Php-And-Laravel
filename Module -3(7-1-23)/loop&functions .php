aa64e2cc76b300c97f2196583e5206a157f97d10
obaydullaa <cr7obaydul@gmail.com>
2023-02-08 00:37:23 +0600

    Multiple Stepping in For Loops.

--- Module -3(7-1-23)/loop&functions .php
+++ Module -3(7-1-23)/loop&functions .php
@@ -1,24 +1,49 @@
 <?php
+/**
+ * 1. Introduction to different types of loops
+ * ==================================================================
+ */
 
-for ($i=1; $i<=5; $i++){
-  // echo $i;
+// for ($i=1; $i<=5; $i++){
+//   // echo $i;
+//   echo PHP_EOL;
+//   for($j=0; $j<$i; $j++) {
+//     echo "*";
+//   }
+// }
+// echo PHP_EOL;
+
+// $i = 0;
+// while($i<=5){
+//   $i++;
+//   echo $i.PHP_EOL;
+// }
+// echo PHP_EOL;
+
+// $i = 0;
+
+// do{
+//   $i++;
+//   echo $i.PHP_EOL;
+// }while($i<5);
+
+// echo "Go To\n";
+
+// $i = 0;
+// a: $i++;
+
+// echo $i.PHP_EOL;
+// if($i<10) goto a;
+
+
+/**
+ * 2. Multiple Stepping in For Loops
+ * ==================================================================
+ */
+
+ for($i = 10, $j=1; $i>0; $i--, $J++) {
+  echo $i.":".$j;
   echo PHP_EOL;
-  for($j=0; $j<$i; $j++) {
-    echo "*";
-  }
-}
-echo PHP_EOL;
-
-$i = 0;
-while($i<=5){
-  $i++;
-  echo $i.PHP_EOL;
-}
-echo PHP_EOL;
-
-$i = 0;
-
-do{
-  $i++;
-  echo $i.PHP_EOL;
-}while($i<5);
+  echo $i.":".(11-$i);
+  echo PHP_EOL;
+ } 
