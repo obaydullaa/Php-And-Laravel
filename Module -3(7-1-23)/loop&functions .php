3843f9e4b3fa473d56b0b950d360006a1210c3c5
obaydullaa <cr7obaydul@gmail.com>
2023-02-10 01:16:08 +0600

    Practice multiple steppping in for loops.

--- Module -3(7-1-23)/loop&functions .php
+++ Module -3(7-1-23)/loop&functions .php
@@ -41,9 +41,26 @@
  * ==================================================================
  */
 
- for($i = 10, $j=1; $i>0; $i--, $J++) {
-  echo $i.":".$j;
-  echo PHP_EOL;
-  echo $i.":".(11-$i);
-  echo PHP_EOL;
- } 
+//  for($i = 10, $j=1; $i>0; $i--, $J++) {
+//   echo $i.":".$j;
+//   echo PHP_EOL;
+//   echo $i.":".(11-$i);
+//   echo PHP_EOL;
+//  } 
+
+/**
+ *  3. More on Multiple Stepping in For Loops
+ */
+
+ for ($i= 0; $i<100; $i++) {
+  //  if($i % 7 == 0) echo $i."\n";
+  //  if($i % 11 == 0) echo $i."\n";
+  // echo $i % 7 == 0 ? $i. "\n" : '';
+  // echo $i % 11 == 0 ? $i. "\n" : '';
+ }
+
+
+ for($i = 0, $j = 0; $i < 100; $i+= 7, $j +=11) {
+  echo $i. "\n";
+  echo $j < 100 ? $j . "\n" : '';
+ }
