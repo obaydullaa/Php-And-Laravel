63acd10e5b1aa038ab38212b6d6d6a70bd111901
obaydullaa <cr7obaydul@gmail.com>
2023-02-11 23:31:02 +0600

    Added git ++ while loop.

--- Module -3(7-1-23)/loop&functions .php
+++ Module -3(7-1-23)/loop&functions .php
@@ -52,15 +52,53 @@
  *  3. More on Multiple Stepping in For Loops
  */
 
- for ($i= 0; $i<100; $i++) {
+//  for ($i= 0; $i<100; $i++) {
   //  if($i % 7 == 0) echo $i."\n";
   //  if($i % 11 == 0) echo $i."\n";
   // echo $i % 7 == 0 ? $i. "\n" : '';
   // echo $i % 11 == 0 ? $i. "\n" : '';
- }
+//  }
 
 
- for($i = 0, $j = 0; $i < 100; $i+= 7, $j +=11) {
-  echo $i. "\n";
-  echo $j < 100 ? $j . "\n" : '';
- }
+//  for($i = 0, $j = 0; $i < 100; $i+= 7, $j +=11) {
+//   echo $i. "\n";
+//   echo $j < 100 ? $j . "\n" : '';
+//  }
+
+/**
+ * 4. Things to keep in mind with ++ in while loops
+ */ 
+
+//  $i = 0;
+//  while($i<5) {
+//   echo $i;
+//   echo PHP_EOL;
+//   $i++;
+//  }
+
+//  echo "============\n";
+
+// $j = 0;
+//  while($j++ <5) {
+//   echo $j;
+//   echo PHP_EOL;
+//  }
+
+//  echo "============\n";
+
+//  $j = 0;
+//  while(++$j<5) {
+//   echo $j;
+//   echo PHP_EOL;
+//  }
+
+//  echo "============\n";
+
+//  $x = $y = 5;
+//  $x = $y++;
+
+//  echo $x.":".$y;
+
+//  $x = $y++;
+//  $x = $y;
+//  $y = $y +1;
