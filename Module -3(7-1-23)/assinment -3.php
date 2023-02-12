4c653683d987b6b75f62440141cf7b643b6c1508
obaydullaa <cr7obaydul@gmail.com>
2023-02-13 00:18:53 +0600

    Assignment summation 1 to 100.

--- Module -3(7-1-23)/assinment -3.php
+++ Module -3(7-1-23)/assinment -3.php
@@ -1,6 +1,6 @@
 <?php
 
-// 1. Write a Reusable  PHP Function that can check Even & Odd Number And Return Decision
+// 1. Write a Reusable PHP Function that can check Even & Odd Number And Return Decision
 
 function evenAndOdd($number) {
   if($number % 2 == 0){
@@ -13,3 +13,13 @@ function evenAndOdd($number) {
 echo evenAndOdd(10); // 10 Is a Even Number.
 echo evenAndOdd(11); // 11 Is a Odd Number.
 
+
+// 2. 1+2+3...…….100  Write a loop to calculate the summation of the series
+$summation = 0;
+$i = 1;
+while ($i <= 100) {
+  $summation += $i;
+  $i++;
+}
+
+echo "summation of the series is $summation"; //summation of the series is 5050
