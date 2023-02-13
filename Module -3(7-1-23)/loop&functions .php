bdbfcb703c807d6d90f5572d7fa051295842d1e2
obaydullaa <cr7obaydul@gmail.com>
2023-02-13 09:28:46 +0600

    Printing Fivonacci using Recursive function

--- Module -3(7-1-23)/loop&functions .php
+++ Module -3(7-1-23)/loop&functions .php
@@ -346,3 +346,25 @@
 // }
 
 // printNumber(10, 20, 5);
+
+/**
+ * 14. Printing Fibonacci Series Using Recursive Functions
+ */
+function fibonacci($old, $new, $end) {
+    static $start;
+    $start = $start ?? 1;
+
+  if($start > $end) {
+    return;
+  }
+  $start ++;
+  echo $old. " ";
+  $_temp = $old + $new;
+  $old = $new;
+  $new = $_temp;
+
+  fibonacci($old, $new, $end);
+
+}
+
+ fibonacci(0,1,10);
