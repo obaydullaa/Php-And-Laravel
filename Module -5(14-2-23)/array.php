99ee8169562cb46ab47246f49954927b284e734e
obaydullaa <cr7obaydul@gmail.com>
2023-02-14 10:39:29 +0600

    Array introduction.

---
+++ Module -5(14-2-23)/array.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * 1. Introduction to arrays
+ * ============================================================================
+ */
+
+  // $students = array (
+  //    "ramhim",
+  //    "karim",
+  //    123,
+  //    "monir",
+  // );
+
+  // $students = [
+  //   "ramhim",
+  //   "karim",
+  //   123,
+  //   "monir",
+  // ];
+
+
+  // echo  $students[0];
+  // var_dump($students); // array print
+
+  // echo count($students); // Howm much element in array
+
+  // $n = count($students);
+
+  // for($i = 0; $i < $n; $i++) {
+  //   echo $students[$i]."\n";
+  // }
+
+  // Reverse Loop
+
+  // for($i = $n -1; $i >= 0; $i--) {
+  //   echo $students[$i]."\n";
+  // }
+
+
+  /**
+ * 1. 2. Array Manipulation
+ * ============================================================================
+ */
+  
