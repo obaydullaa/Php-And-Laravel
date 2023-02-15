42290bb33e9691aa3694b761080c5e4ddb14cf23
obaydullaa <cr7obaydul@gmail.com>
2023-02-15 09:37:19 +0600

    array element add & remove.

--- Module -5(14-2-23)/array.php
+++ Module -5(14-2-23)/array.php
@@ -23,7 +23,7 @@
   // echo  $students[0];
   // var_dump($students); // array print
 
-  // echo count($students); // Howm much element in array
+  // echo count($students); // How much element in array
 
   // $n = count($students);
 
@@ -39,23 +39,42 @@
 
 
   /**
- * 1. 2. Array Manipulation
+ * 2. Array Manipulation
  * ============================================================================
  * array is mutable = array is  modified.
  * Immutable na mane modified kora jai.
  */
   
    $students = [
-    "ramhim",
+    "rahim",
     "karim",
-    123,
+    "Rifat",
     "monir",
-
   ];
 
-  $students[2] = 'Rohim';
+  // $students[2] = 'Rohim';
   
+  echo PHP_EOL;
+  
+  echo array_shift($students);
+  echo PHP_EOL;
+  echo array_pop($students);
+  echo PHP_EOL;
+  echo PHP_EOL;
+
+  echo array_unshift($students,'rahim');
+  echo array_push($students,'monir');
+
   $n = count($students);
   for($i = 0; $i < $n; $i++) {
     echo $students[$i]."\n";
   }
+
+  
+  echo PHP_EOL;
+
+
+  // array_shift($students);              // Cut first array element.
+  // array_pop($students);                // Cut last array element.
+  // array_unshift($students, 'element'); // Add first element
+  // array_push($students, 'element');    // Add last element
