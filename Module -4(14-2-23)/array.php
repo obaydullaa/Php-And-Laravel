e578626d9f8a478fb1259677154d5ae309ff191b
obaydullaa <cr7obaydul@gmail.com>
2023-02-21 18:48:17 +0600

    value check conditions.

--- Module -4(14-2-23)/array.php
+++ Module -4(14-2-23)/array.php
@@ -322,3 +322,31 @@
 // unset($person['lname']);
 
 // print_r($person);
+
+
+/**
+ * 9. Discussion of Empty Values ​​(Important)
+ * =========================================================================
+ */
+
+//  $name =0;
+
+//  if(isset($name)) {
+//   echo "Name is set";
+//  }else {
+//   echo "Not Set";
+//  }
+
+//  echo PHP_EOL;
+//  if(empty($name)) {
+//   echo "Name is Empty";
+//  }else {
+//   echo "Not Empty";
+//  }
+//  echo "\n";
+
+//  if(isset($name) && (is_numeric($name) || $name != '')){
+//    echo 'Name is set it\'s not empty';
+//  } else {
+//   echo 'Name is either not set or it\'s empty';
+//  }
