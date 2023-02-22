c9c6f9262e97fa83971eaf6260ed990005b3f3ca
obaydullaa <cr7obaydul@gmail.com>
2023-02-23 00:28:29 +0600

    array map walk map and filer .

--- Module -4(14-2-23)/array.php
+++ Module -4(14-2-23)/array.php
@@ -516,3 +516,50 @@
 
 //  print_r($diff);
 //  print_r($difff);
+
+/**
+ * 16. Array Utility Functions - Very Important - Walk, Map and Filter
+ * =================================================================================
+ * array_walk($numbers, 'square'); // protita function ar element ar upore kaj kore.... // not modify main arry
+ * array_walk echo or onno kono kaj ar jonno use hoi... eta return kore kono lav nai.
+ * array_map('cube', $numbers); // protita element ar upore kaj kore and eta return o kore.
+ * array_filter($persons, 'filterBys'); // every element niye kaj kore
+ * 
+ */
+
+//  $numbers =[1,2,3,4,5,6,7,8,9,10,11,12];
+
+//  function square($n) {
+//   printf("Square of %d is %d \n", $n, $n*$n);
+//  }
+//  $newArray = array_walk($numbers, 'square');
+
+
+//  function cube($n) {
+//   return $n*$n*$n;
+//  }
+
+//  $newArray = array_map('cube', $numbers);
+
+//  function even($n) {
+//   return $n%2== 0;
+//  }
+//  function odd($n) {
+//   return $n%2== 1;
+//  }
+
+//  $evenNumbers = array_filter( $numbers, 'even'); // call back function
+//  $oddNumbers = array_filter( $numbers, 'odd'); // call back function
+
+//  print_r($evenNumbers);
+//  print_r($oddNumbers);
+
+//  $persons = ['sujon','kabir','sabab','selim','roni','jamal','kamal','sayma'];
+
+//  function filterBys($name) {
+//     return $name[0]=='s';
+//  }
+
+//  $newPersons = array_filter($persons, 'filterBys');
+
+//  print_r($newPersons);
