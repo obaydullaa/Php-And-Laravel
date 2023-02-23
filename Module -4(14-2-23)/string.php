cc36a291fc8ff57223b304c1c8cfc87832a20858
obaydullaa <cr7obaydul@gmail.com>
2023-02-24 01:27:40 +0600

    Reverse string .

--- Module -4(14-2-23)/string.php
+++ Module -4(14-2-23)/string.php
@@ -54,3 +54,28 @@
 // echo PHP_EOL;
 
 // echo substr($string, -10, -8);
+
+
+/**
+ * 25 Reversing strings
+ * ========================================
+ */
+
+ $string = "Hello World";
+
+
+ $length = strlen($string)-1;
+
+ for($i=$length; $i>=0; $i--){
+    echo $string[$i];
+ }
+
+ echo PHP_EOL;
+
+ for($i =1; $i<=$length; $i++){
+    echo $string[$i* -1];
+ }
+
+ echo PHP_EOL;
+
+echo strrev($string);
