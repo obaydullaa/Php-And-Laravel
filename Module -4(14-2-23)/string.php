3ce450da696607c9e3ec121954f11ff5f0e074e4
obaydullaa <cr7obaydul@gmail.com>
2023-02-24 01:14:40 +0600

    string file added.

---
+++ Module -4(14-2-23)/string.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * 22 How to Write Strings Differently in PHP
+ * ===========================================================
+ *  <<<EOD ....... EOD; // HereDoc
+ * <<<'EOD' ....... EOD; // NowDoc
+ */
+
+//  $name = "Rakib";
+
+//  $string01 = 'My name is $name';
+//  echo $string01;
+
+//  $heredoc = <<<EOD
+
+//   Data One
+//   My name jis $name \n
+//   Data Twoooooooooooo
+//   EOD;
+
+//  echo $heredoc;
+
+/**
+ * 23 Introduction to ASCII Code
+ * ==============================================================
+ * American Standard Code for Information Interchange,
+ *  ord('A'); // Convert the first byte of a string to a value between 0 and 255
+ * chr(99); // Generate a single-byte string from a number
+ */
+
+//  echo ord('A');
+//  echo PHP_EOL;
+//  echo ord('a');
+//  echo PHP_EOL;
+//  echo chr(99);
+
+
+/**
+ * 24 Accessing characters within a string
+ * =============================================================
+ * substr($string,1,4)
+ * substr($string, -3)
+ */
+
+//  $string = 'Hellow World';
+
+// //  echo $string[-5];
+// //  echo $string[5];
+
+// $length = strlen($string);
+// // echo substr($string,1,4);
+// echo substr($string,$length-3);
+
+// echo PHP_EOL;
+
+// echo substr($string, -10, -8);
