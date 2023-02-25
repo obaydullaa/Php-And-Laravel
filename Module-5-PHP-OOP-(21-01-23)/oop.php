086972eb1ea67e35d437a0e7bc0f7bc29e72c1e7
obaydullaa <cr7obaydul@gmail.com>
2023-02-25 07:58:19 +0600

    __constructor method use

--- Module-5-PHP-OOP-(21-01-23)/oop.php
+++ Module-5-PHP-OOP-(21-01-23)/oop.php
@@ -13,37 +13,37 @@
  * 
  */
 
-class Human {
-    public $name;
-    function sayHi() {
-        echo "Salam \n";
-        $this->sayName();
+// class Human {
+//     public $name;
+//     function sayHi() {
+//         echo "Salam \n";
+//         $this->sayName();
 
-    }
+//     }
 
-    private function sayName(){
-        echo "My Name is {$this->name}\n";
-    }
-}
+//     private function sayName(){
+//         echo "My Name is {$this->name}\n";
+//     }
+// }
 
-class Cat{
-    function sayHi() {
-        echo "Mewo \n";
-    }
-}
+// class Cat{
+//     function sayHi() {
+//         echo "Mewo \n";
+//     }
+// }
 
-class Dog{
-    function sayHi() {
-        echo "Woof \n";
-    }
-}
+// class Dog{
+//     function sayHi() {
+//         echo "Woof \n";
+//     }
+// }
 
-$h1 = new Human();
-$h2 = new Human();
-$h1->name = "Ruble"; // set
-$h2->name = "Obaydul";
-$c1 = new Cat();
-$d1 = new Dog();
+// $h1 = new Human();
+// $h2 = new Human();
+// $h1->name = "Ruble"; // set
+// $h2->name = "Obaydul";
+// $c1 = new Cat();
+// $d1 = new Dog();
 
 // $h1->sayName();
 // $h1->sayName();
@@ -51,7 +51,45 @@ $d1 = new Dog();
 // $c1->sayHi();
 
 
-$h2->sayHi();
+// $h2->sayHi();
 // $h2->sayName();
 // echo $h2->name; // get
 // $c1->sayHi();
+
+/**
+ * 3 A class's constructor method that runs automatically when an object is created from the class
+ * ================================================================================
+ * 
+ */
+
+ class Human {
+    public $name;
+    public $age;
+    function __construct($personName,$personAge=0) {
+        // echo "New Human Object is Created. \n";
+        $this->name = $personName;
+        $this->age = $personAge;
+
+    }
+
+    function sayHi(){
+        echo "Salam\n";
+        $this->sayName();
+    }
+
+    function sayName(){
+        if($this->age){
+            echo "My Name is {$this->name}, I am {$this->age} year old\n";
+        }else {
+            echo "My Name is {$this->name}, I don't know how old i am. \n";
+        }
+    }
+}
+
+$h1 = new Human("Obaydul",22);
+$h2 = new Human("Rifat",13);
+$h3 = new Human("Noor");
+// $h1->name = "Obaydul";
+$h1->sayHi();
+$h2->sayHi();
+$h3->sayHi();
