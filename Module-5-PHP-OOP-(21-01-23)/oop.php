dc86bb3c9bf0c833d9f3bc1d9babfcdff310320b
obaydullaa <cr7obaydul@gmail.com>
2023-02-26 09:01:07 +0600

    examples inheritance.

--- Module-5-PHP-OOP-(21-01-23)/oop.php
+++ Module-5-PHP-OOP-(21-01-23)/oop.php
@@ -191,25 +191,77 @@
  * ============================================================================
  */
 
- class ParentClass {
-    protected $name;
-    function __construct($name){
-        $this->name = $name;
-        $this->sayHi();
-    }
+//  class ParentClass {
+//     protected $name;
+//     function __construct($name){
+//         $this->name = $name;
+//         $this->sayHi();
+//     }
 
-    function sayHi () {
-        echo "Hi from {$this->name}\n";
-    }
+//     function sayHi () {
+//         echo "Hi from {$this->name}\n";
+//     }
 
- }
+//  }
 
- class ChildClass extends ParentClass {
+//  class ChildClass extends ParentClass {
     
-    function sayHi () {
-        parent::sayHi();
-        echo "Hello\n";
-    }
- }
+//     function sayHi () {
+//         parent::sayHi();
+//         echo "Hello\n";
+//     }
+//  }
 
- $cc = new ChildClass("ABCD"); 
+//  $cc = new ChildClass("ABCD"); 
+
+/**
+ * 7 More examples on inheritance
+ * ======================================================
+ */
+
+//  class shape {
+//     protected $name;
+//     protected $area;
+//     public function __construct($name) {
+//         $this->name = $name;
+//         $this->calculateArea();
+//     }
+//     public function getArea() {
+//         echo "This {$this->name}'s area is {$this->area} \n";
+//     }
+
+//     public function calculateArea() {}
+//  }
+
+//  class Triangle extends Shape {
+//     private $a, $b;
+//     public function __construct($a, $b, $c) {
+//         $this->a = $a;
+//         $this->b = $b;
+//         $this->c = $c;
+//         parent::__construct("Triangle");
+//     }
+
+//     public function calculateArea() {
+//         $perimeter = ($this->a+$this->b+$this->c)/2;
+//         $this->area = sqrt($perimeter*($perimeter-$this->a)*($perimeter-$this->b)*($perimeter-$this->c));
+//     }
+//  }
+
+//  class Rectangle extends Shape{
+//     private $a, $b;
+//     public function __construct( $a, $b) {
+//         $this->a = $a;
+//         $this->b = $b;
+//         parent::__construct("Rectangle");
+//     }
+//     public function calculateArea() {
+//         $this->area = $this->a * $this->b;
+//     }
+//  }
+
+//  $r = new Rectangle(2,4); 
+//  $r->getArea();
+
+//  $t = new Triangle(10, 12, 8);
+//  $t->getArea();
