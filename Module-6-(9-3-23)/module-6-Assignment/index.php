2f3d91e91b7c922299711844d2129e9a30f1badd
obaydullaa <cr7obaydul@gmail.com>
2023-03-13 22:47:40 +0600

    Added style css.

--- Module-6-(9-3-23)/module-6-Assignment/index.php
+++ Module-6-(9-3-23)/module-6-Assignment/index.php
@@ -2,24 +2,29 @@
 <html>
   <head>
     <title>Registration Form</title>
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <h1>Registration Form</h1>
-    <form action="register.php" method="POST" enctype="multipart/form-data">
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="name" required><br>
 
-      <label for="email">Email:</label>
-      <input type="email" id="email" name="email" required><br>
+    <div class="reg-wrapper">
+      <h1>Registration Form</h1>
+      <form action="register.php" method="POST" enctype="multipart/form-data">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br>
 
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
 
-      <label for="profile_picture">Profile Picture:</label>
-      <input type="file" id="profile_picture" name="profile_picture" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
 
-      <input type="submit" value="Submit">
-    </form>
+        <label for="profile_picture">Profile Picture:</label>
+        <input type="file" id="profile_picture" name="profile_picture" required><br>
+
+        <input class="btn" type="submit" value="Submit">
+      </form>
+    </div>
+
   </body>
 </html>
 
