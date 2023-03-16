8c11f0060404b247cd8eb2649a6bc3298868f709
obaydullaa <cr7obaydul@gmail.com>
2023-03-16 09:52:07 +0600

    Added dateTime ()

--- Module-6-(9-3-23)/file_session_date-(Live-cls)/diff.php
+++ Module-6-(9-3-23)/file_session_date-(Live-cls)/diff.php
@@ -1,9 +1,36 @@
 <?php
+
 /**
- * // 
- * strotime();
+ * // january 1972 all seconds
+ * strtotime();
  */
 
-$date = strtotime('15th March, 2023');
+// $date1 = strtotime('1st january 2023');
+// $date2= strtotime('28th january 2023');
 
-echo $date;
+// echo $date2-$date1;
+
+$date1 = new DateTime('1st january 2021');
+$date2= new DateTime('28th march 2023');
+
+$diff = $date1->diff($date2);
+
+// echo $diff->d;
+
+// echo $diff->y>0?$diff->y." Year": '';
+// echo $diff->m>0?$diff->m." Month": '';
+// echo $diff->d>0?$diff->m." Day": '';
+
+echo formatNumber($diff->y,'year').
+formatNumber($diff->m,'month') .
+formatNumber($diff->m,'day') 
+;
+
+function formatNumber ($number, $type) {
+    if($number == 0) {
+        return '';
+    }
+    if($number >1) {
+        return " {$number} {$type}s";
+    }
+}
