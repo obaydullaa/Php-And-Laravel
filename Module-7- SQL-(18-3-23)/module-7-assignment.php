f127597f163950b34ea9cf9ff26cabc50e7245fa
obaydullaa <cr7obaydul@gmail.com>
2023-03-22 02:03:38 +0600

    practice sql .

--- Module-7- SQL-(18-3-23)/module-7-assignment.php
+++ Module-7- SQL-(18-3-23)/module-7-assignment.php
@@ -56,7 +56,7 @@ CREATE TABLE departments (
   //========================================================================================
   
 // a. Write a query to select all columns and rows from the employees table.
-SELECT * FROM employees;
+SELECT * FROM employees; 
 
 // b. Write a query to select only the name and salary columns of all employees with a salary greater than 50000.
 SELECT name, salary FROM employees WHERE salary > 50000;
@@ -68,7 +68,7 @@ SELECT AVG(salary) FROM employees;
 SELECT COUNT(*) FROM employees WHERE department_id = (SELECT id FROM departments WHERE name = 'Marketing');
 
 // e. Write a query to update the salary column of the employee with an id of 1001 to 60000.
-UPDATE employees SET salary = 60000 WHERE id = 1001;
+UPDATE employees SET salary = 60000 WHERE id = 1001; 
 
 // f. Write a query to delete all employees whose salary is less than 30000.
 DELETE FROM employees WHERE salary < 30000;
