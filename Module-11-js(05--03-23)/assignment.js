440cdca23e1fb358c1166ba81b87c9f70dedc487
obaydullaa <cr7obaydul@gmail.com>
2023-04-18 23:40:01 +0600

    Added module  11 assignment file.

---
+++ Module-11-js(05--03-23)/assignment.js
@@ -0,0 +1,8 @@
+// Question : Write a program that generates a multiplication table for a given number using a for loop.
+
+// Instructions:
+// Create a JavaScript function called "multiplicationTable" that takes a single argument "num" (an integer) as input.
+// Inside the function, create a for loop that iterates from 1 to 10.
+// Inside the loop, multiply the current iteration number by the input "num" to generate the product.
+// Log the product to the console in the format "num x iteration = product".
+// Test the function with different input numbers to generate multiplication tables.~
