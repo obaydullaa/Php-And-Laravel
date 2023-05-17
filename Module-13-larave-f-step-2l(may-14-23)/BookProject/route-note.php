6ba9f4fb182a4a9ff9212d274bc9311fac758d28
obaydullaa <cr7obaydul@gmail.com>
2023-05-17 23:46:07 +0600

    Laravel rout note added.

--- Module-13-larave-f-step-2l(may-14-23)/BookProject/route-note.php
+++ Module-13-larave-f-step-2l(may-14-23)/BookProject/route-note.php
@@ -8,3 +8,84 @@ DELETE: Delete a record.
 All call are VERB;
 
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function () {
+    echo "Hello World";
+});
+
+// Route::get('/books',[BookController::class,'books']);
+// Route::get('/books/{id}',[BookController::class,'getBook']);
+// Route::get('/books/{id}/{field}',[BookController::class,'getBookField']);
+
+Route::controller(BookController::class)->group(function(){
+    Route::get('/books','books');
+    Route::get('/books/{id}','getBook');
+    Route::get('/books/{id}/{field}','getBookField');
+
+    Route::post('/books', 'createBook');
+});
+
+// Route::get('/books/{id}',[BookController::class,'getBook'])-> whereNumber('id');
+// Route::get('/books/{id}/author',[BookController::class,'getBookAuthor']);
+// Route::get('/books/{id}/title',[BookController::class,'getBookAuthor']);
+// Route::get('/weather/{city?}', [WeatherController::class, 'getWeather']);
+
+// Controller class
+//====================================================
+
+class BookController extends Controller
+{
+    private $books = [
+        [
+            'author' => 'Jane Austen',
+            'title'  => 'Pride and Prejudice'
+        ],
+        [
+            'author' => 'f. Scott Fitzgerold',
+            'title'  => 'The Freat Gatsby'
+        ],
+        [
+            'author' => 'George Orwell',
+            'title'  => '1984'
+        ],
+        [
+            'author' => 'J.R.R Tolkien',
+            'title'  => 'The lord of the Rings'
+        ],
+        [
+            'author' => 'Hoper Lee',
+            'title'  => 'To kill a Mockingbird'
+        ],
+    ];
+
+    public function books()
+    {
+        return $this->books;
+    }
+
+    public function getBook($id)
+    {
+        $bookId =$id - 1;
+        return $this->books[$bookId];
+    }
+    // public function getBookAuthor($id)
+    // {
+    //     $bookId =$id - 1;
+    //     return $this->books[$bookId];
+    // }
+    public function getBookField($id, $field)
+    {
+        $bookId =$id - 1;
+        $book = $this->books[$bookId];
+        return $book[$field];
+    }
+    public function createBook()
+    {
+        
+        return "New post Request.";
+    }
+}
