4682ce66224d4d68276302cb37e4a7adb8dcd785
obaydullaa <cr7obaydul@gmail.com>
2023-05-22 23:27:33 +0600

    Added assignment project.

--- Module-14-larave-f-step-13(may-18-23)/assignment-project/routes/web.php
+++ Module-14-larave-f-step-13(may-18-23)/assignment-project/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/submit', function (Request $request):string {
+    $email = $request->input('email');
+
+    // Store the email or perform any desired operations
+    $response = [
+        'success' => true,
+        'message' => 'Form submitted successfully.'
+    ];
+
+    return response()->json($response);
 });
