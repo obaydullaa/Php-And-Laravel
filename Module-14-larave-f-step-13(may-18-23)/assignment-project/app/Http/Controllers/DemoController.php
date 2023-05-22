d947d02df15a01835a036d6b812ece53aefa8106
obaydullaa <cr7obaydul@gmail.com>
2023-05-23 00:18:03 +0600

    Header data added.

--- Module-14-larave-f-step-13(may-18-23)/assignment-project/app/Http/Controllers/DemoController.php
+++ Module-14-larave-f-step-13(may-18-23)/assignment-project/app/Http/Controllers/DemoController.php
@@ -7,10 +7,15 @@ use Illuminate\Http\Request;
 class DemoController extends Controller
 {
     public function name(Request $request)
-{
+    {
 
-    $name = $request->input('name');
-    
-    return response()->json(['message' => 'Name retrieved successfully', 'name' => $name]);
-}
+        $name = $request->input('name');
+        $userAgent = request()->header('User-Agent');
+        return response()->json(
+            ['message' => 'Name retrieved successfully', 
+            'name' => $name,
+            'userAgent' => $userAgent
+            ]
+        );
+    }
 }
