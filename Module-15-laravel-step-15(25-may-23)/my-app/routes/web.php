265ae5554d3539e68622533f5c05ed03cedaa40c
obaydullaa <cr7obaydul@gmail.com>
2023-05-27 23:54:48 +0600

    Create about controller.

--- Module-15-laravel-step-15(25-may-23)/my-app/routes/web.php
+++ Module-15-laravel-step-15(25-may-23)/my-app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\demoController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -30,6 +31,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', ([HomeController::class, 'Home']));
+Route::get('/about', ([AboutController::class, 'About']));
+
 Route::get('/hello', function () {
     // return view('welcome');
     echo 'welcome';
