5ec4094c48ec388c35758212d1b2583cbcd98c86
obaydullaa <cr7obaydul@gmail.com>
2023-05-27 23:57:22 +0600

    about blade create.

---
+++ Module-15-laravel-step-15(25-may-23)/my-app/resources/views/pages/About.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+
+    @include('component.About')
+    @include('component.WhatWeDo')
+    @include('component.Experience')
+    @include('component.Subscribe')
+    @include('component.Testimonials')
+    @include('component.Blog')
+    @include('component.Team')
+
+@endsection
