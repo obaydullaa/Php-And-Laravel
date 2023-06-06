b0b765151d0b68b251ebb531b862bea822e5ec56
obaydullaa <cr7obaydul@gmail.com>
2023-06-07 00:06:12 +0600

    table create use migration.

--- Module-16-laravel-step-16(30-may-23)/migration-app/database/migrations/2023_06_06_175214_create_profiles.php
+++ Module-16-laravel-step-16(30-may-23)/migration-app/database/migrations/2023_06_06_175214_create_profiles.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('city');
             $table->timestamps();
         });
     }
