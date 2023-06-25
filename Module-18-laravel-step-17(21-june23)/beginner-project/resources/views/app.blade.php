47fa6021b1c11d0dcfeac61afaeb608ccab29ae4
obaydullaa <cr7obaydul@gmail.com>
2023-06-25 08:50:43 +0600

    Added views file structure.

---
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>MR-X</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
+        <link href="css/bootstrap.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+
+    <body class="d-flex flex-column h-100">
+
+
+
+
+
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
