99f7894d43a17ea632552675f4024417fee3aeda
obaydullaa <cr7obaydul@gmail.com>
2023-06-27 00:54:17 +0600

    Added  all assets.

--- Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/app.blade.php
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/app.blade.php
@@ -1,29 +1,32 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="light">
 
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>MR-X</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
-        <link href="css/bootstrap.css" rel="stylesheet" />
-        <link href="css/style.css" rel="stylesheet" />
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>MR-X</title>
+    <link rel="icon" type="image/x-icon" href="{{asset('/favicon.ico')}}" />
+    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
+</head>
 
-    <body class="d-flex flex-column h-100">
+<body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
 
-    @include('components.navbar')
-    @include('components.loader')
+        @include('components.navbar')
+        @include('components.loader')
 
-    <div class="" id="content-div">
-        @yield('content')
-    </div>
+        <div class="" id="content-div">
+            @yield('content')
+        </div>
 
-    @include('components.footer')
+        @include('components.footer')
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    </body>
+        <script src="{{asset('js/axios.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+</body>
+
 </html>
