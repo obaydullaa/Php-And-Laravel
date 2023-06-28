af8f186a3957c4aa8e831bed4aeede4b00800aa3
obaydullaa <cr7obaydul@gmail.com>
2023-06-29 01:36:09 +0600

    all blade html file added.

---
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/hero.blade.php
@@ -0,0 +1,25 @@
+<header class="py-5">
+    <div class="container px-5 pb-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-xxl-5">
+                <!-- Header text content-->
+                <div class="text-center text-xxl-start">
+                    <div class="badge bg-gradient-primary-to-secondary text-white mb-4"><div class="text-uppercase">Design &middot; Development &middot; Marketing</div></div>
+                    <div class="fs-3 fw-light text-muted">I can help your business to</div>
+                    <h1 class="display-3 fw-bolder mb-5"><span class="text-gradient d-inline">Get online and grow fast</span></h1>
+                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-3">
+                        <a class="btn btn-primary btn-lg px-5 py-3 me-sm-3 fs-6 fw-bolder" href="resume.html">Resume</a>
+                        <a class="btn btn-outline-dark btn-lg px-5 py-3 fs-6 fw-bolder" href="projects.html">Projects</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-7">
+                <div class="d-flex justify-content-center mt-5 mt-xxl-0">
+                    <div class="profile">
+                        <img class="profile-img" src="assets/profile.png" alt="..." />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/loader.blade.php
@@ -0,0 +1,6 @@
+
+<div id="loading-div" style="display: none">
+    <div id="spinner-container">
+        <div id="loading-spinner"></div>
+    </div>
+</div>
