3a7df7ce7e82f9d573d86e7785cbc18c8d882912
obaydullaa <cr7obaydul@gmail.com>
2023-06-29 00:44:54 +0600

    Added about blade html.

---
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/about.blade.php
@@ -0,0 +1,19 @@
+<!-- About Section-->
+<section class="bg-light py-5">
+    <div class="container px-5">
+        <div class="row gx-5 justify-content-center">
+            <div class="col-xxl-8">
+                <div class="text-center my-5">
+                    <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+                    <p class="lead fw-light mb-4">My name is Start Bootstrap and I help brands grow.</p>
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?</p>
+                    <div class="d-flex justify-content-center fs-2 gap-4">
+                        <a class="text-gradient" href="#!"><i class="bi bi-twitter"></i></a>
+                        <a class="text-gradient" href="#!"><i class="bi bi-linkedin"></i></a>
+                        <a class="text-gradient" href="#!"><i class="bi bi-github"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
