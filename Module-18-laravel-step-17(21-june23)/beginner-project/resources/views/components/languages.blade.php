2500fef81299cf7608a5c43b1e5f11faa8ff38d5
obaydullaa <cr7obaydul@gmail.com>
2023-06-30 01:27:29 +0600

    Added all blade templating complete.

--- Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/languages.blade.php
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/languages.blade.php
@@ -1,71 +1,49 @@
+<!-- Page Content-->
+<div class="container px-5 my-5">
+    <div class="row gx-5 justify-content-center">
+        <div class="col-lg-11 col-xl-9 col-xxl-8">
 
-    <!-- Page Content-->
-    <div class="container px-5 my-5">
-        <div class="row gx-5 justify-content-center">
-            <div class="col-lg-11 col-xl-9 col-xxl-8">
-                
-                <div class="pb-5"></div>
-                <!-- Skills Section-->
-                <section>
-                    <!-- Skillset Card-->
-                    <div class="card shadow border-0 rounded-4 mb-5">
-                        <div class="card-body p-5">
-                            <!-- Languages list-->
-                            <div class="mb-0">
-                                <div class="d-flex align-items-center mb-4">
-                                    <div class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3"><i class="bi bi-code-slash"></i></div>
-                                    <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
+            <div class="pb-5"></div>
+            <!-- Skills Section-->
+            <section>
+                <!-- Skillset Card-->
+                <div class="card shadow border-0 rounded-4 mb-5">
+                    <div class="card-body p-5">
+                        <!-- Languages list-->
+                        <div class="mb-0">
+                            <div class="d-flex align-items-center mb-4">
+                                <div
+                                    class="feature bg-primary bg-gradient-primary-to-secondary text-white rounded-3 me-3">
+                                    <i class="bi bi-code-slash"></i>
                                 </div>
-                                <div class="row row-cols-1 row-cols-md-3 mb-4">
-                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
-                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div></div>
-                                    <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div></div>
+                                <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
+                            </div>
+                            <div class="row row-cols-1 row-cols-md-3 mb-4">
+                                <div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div>
                                 </div>
-                                <div class="row row-cols-1 row-cols-md-3">
-                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div></div>
-                                    <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Ruby</div></div>
-                                    <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Node.js</div></div>
+                                <div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div>
+                                </div>
+                                <div class="col">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div>
+                                </div>
+                            </div>
+                            <div class="row row-cols-1 row-cols-md-3">
+                                <div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div>
+                                </div>
+                                <div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Ruby</div>
+                                </div>
+                                <div class="col">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Node.js</div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>
-    
-            </div>
+                </div>
+            </section>
+
         </div>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-
-
-
-
-
-
-
-
-
-
-
-   
-
-
+    </div>
