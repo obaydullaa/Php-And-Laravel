6b295cce3440cb71759dd1644a59799fbf4ac097
obaydullaa <cr7obaydul@gmail.com>
2023-07-01 00:16:12 +0600

    Added porject list.

--- Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/project-list.blade.php
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/project-list.blade.php
@@ -5,9 +5,26 @@
             <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">Projects</span></h1>
         </div>
         <div class="row gx-5 justify-content-center">
-            <div class="col-lg-11 col-xl-9 col-xxl-8">
+            <div id="project-list" class="col-lg-11 col-xl-9 col-xxl-8">
+
+
                 <!-- Project Card 1-->
-                <div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+               
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<script>
+
+    async getProjectList () {
+        let URL =  "/projectsData"; 
+
+        try{
+            const response = await axios.get(URL);
+            response.data.forEach((item)=>{
+                document.getElementById('project-list').innerHTML +=(`<div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
                     <div class="card-body p-0">
                         <div class="d-flex align-items-center">
                             <div class="p-5">
@@ -17,20 +34,15 @@
                             <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
                         </div>
                     </div>
-                </div>
-                <!-- Project Card 2-->
-                <div class="card overflow-hidden shadow rounded-4 border-0">
-                    <div class="card-body p-0">
-                        <div class="d-flex align-items-center">
-                            <div class="p-5">
-                                <h2 class="fw-bolder">Project Name 2</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius at enim eum illum aperiam placeat esse? Mollitia omnis minima saepe recusandae libero, iste ad asperiores! Explicabo commodi quo itaque! Ipsam!</p>
-                            </div>
-                            <img class="img-fluid" src="https://dummyimage.com/300x400/343a40/6c757d" alt="..." />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+                </div>`)
+
+
+            })
+
+
+        }catch(error){
+            alert(error);
+        }
+    }
+
+</script>
