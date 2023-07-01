935cce70f0f3e091c1ab95d4f54ca2f6b7102c3b
obaydullaa <cr7obaydul@gmail.com>
2023-07-02 00:43:19 +0600

    Languages blade dynamic and show axios.

--- Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/languages.blade.php
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/languages.blade.php
@@ -18,27 +18,8 @@
                                 </div>
                                 <h3 class="fw-bolder mb-0"><span class="text-gradient d-inline">Languages</span></h3>
                             </div>
-                            <div class="row row-cols-1 row-cols-md-3 mb-4">
-                                <div class="col mb-4 mb-md-0">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div>
-                                </div>
-                                <div class="col mb-4 mb-md-0">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div>
-                                </div>
-                                <div class="col">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div>
-                                </div>
-                            </div>
-                            <div class="row row-cols-1 row-cols-md-3">
-                                <div class="col mb-4 mb-md-0">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div>
-                                </div>
-                                <div class="col mb-4 mb-md-0">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Ruby</div>
-                                </div>
-                                <div class="col">
-                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Node.js</div>
-                                </div>
+                            <div id="skill-list" class="row row-cols-1 row-cols-md-3 mb-4">
+                                
                             </div>
                         </div>
                     </div>
@@ -47,3 +28,25 @@
 
         </div>
     </div>
+
+    <script>
+        LanguagesList();
+        async function LanguagesList() {
+            try {
+                let URL="/languageData";
+                let response = await axios.get(URL);
+
+                response.data.forEach((item)=>{
+                    document.getElementById('skill-list').innerHTML +=(`<div class="col mb-4 mb-md-0">
+                                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">${item['name']}</div>
+                                </div>`)
+                })
+
+            } catch(error) {
+                alert(error); 
+            }
+            
+        }
+
+
+    </script>
