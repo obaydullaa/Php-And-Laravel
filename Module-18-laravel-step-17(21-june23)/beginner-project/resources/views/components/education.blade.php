e7e1abea66d49ec707a2937a30a34fe30e7883d3
obaydullaa <cr7obaydul@gmail.com>
2023-07-02 16:19:53 +0600

    Added education blade show with ajax.

--- Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/education.blade.php
+++ Module-18-laravel-step-17(21-june23)/beginner-project/resources/views/components/education.blade.php
@@ -8,49 +8,10 @@
                   <!-- Education Section-->
                   <section>
                     <h2 class="text-secondary fw-bolder mb-4">Education</h2>
-                    <!-- Education Card 1-->
-
-                    <div class="card shadow border-0 rounded-4 mb-5">
-                        <div class="card-body p-5">
-                            <div class="row align-items-center gx-5">
-                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
-                                    <div class="bg-light p-4 rounded-4">
-                                        <div class="text-secondary fw-bolder mb-2">2015 - 2017</div>
-                                        <div class="mb-2">
-                                            <div class="small fw-bolder">Barnett College</div>
-                                            <div class="small text-muted">Fairfield, NY</div>
-                                        </div>
-                                        <div class="fst-italic">
-                                            <div class="small text-muted">Master's</div>
-                                            <div class="small text-muted">Web Development</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-8"><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Education Card 2-->
-                    <div class="card shadow border-0 rounded-4 mb-5">
-                        <div class="card-body p-5">
-                            <div class="row align-items-center gx-5">
-                                <div class="col text-center text-lg-start mb-4 mb-lg-0">
-                                    <div class="bg-light p-4 rounded-4">
-                                        <div class="text-secondary fw-bolder mb-2">2011 - 2015</div>
-                                        <div class="mb-2">
-                                            <div class="small fw-bolder">ULA</div>
-                                            <div class="small text-muted">Los Angeles, CA</div>
-                                        </div>
-                                        <div class="fst-italic">
-                                            <div class="small text-muted">Undergraduate</div>
-                                            <div class="small text-muted">Computer Science</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-8"><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
-                            </div>
-                        </div>
+                    
+                    <div id="educational-list">
+                        <!-- Education Card 1-->
+                        
                     </div>
 
                 </section>
@@ -60,4 +21,41 @@
     </div>
 </div>
    
+<script>
+    getEducList()
+
+    async function getEducList() {
+        try{
+            let URL = "/educationData";
+            let response = await axios.get(URL);
+
+            response.data.forEach((item)=>{
+                document.getElementById('educational-list').innerHTML +=(`<div class="card shadow border-0 rounded-4 mb-5">
+                            <div class="card-body p-5">
+                                <div class="row align-items-center gx-5">
+                                    <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                        <div class="bg-light p-4 rounded-4">
+                                            <div class="text-secondary fw-bolder mb-2">${item['duration']}</div>
+                                            <div class="mb-2">
+                                                <div class="small fw-bolder">${item['institutionName']}</div>
+                                            </div>
+                                            <div class="fst-italic">
+                                                <div class="small text-muted">${item['field']}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-8"><div>${item['details']}</div></div>
+                                </div>
+                            </div>
+                        </div>`)
+
+            })
+                
+            }catch(error) {
+                alert(error);
+            }
+    }
+
+
+</script>
    
