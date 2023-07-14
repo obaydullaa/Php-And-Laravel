16b971215fa389fb17c4fb251661dcbe54a10793
obaydullaa <cr7obaydul@gmail.com>
2023-07-14 17:24:55 +0600

    Added blog comporent showing axios.

--- Module-19-laravel-step-18(09-july-23)/blog-project-Orm/resources/views/components/blog.blade.php
+++ Module-19-laravel-step-18(09-july-23)/blog-project-Orm/resources/views/components/blog.blade.php
@@ -22,6 +22,49 @@
 <script src="{{ asset('js/jquery-3.6.1.min.js') }}"></script>
 <script src="{{ asset('js/axios.min.js') }}"></script>
 
+{{-- @section('script') --}}
 
+<script>
+    (async () => {
+        try{
+            const URL = "{{ url('/allBlog')}}";
+            const response = await axios.get(URL);
+
+            console.log(response.data['title']);
+
+            response.data.forEach((item)=>{
+                console.log(item);
+                document.getElementById('blog-item').innerHTML+=(` <div class="col-lg-4 col-md-6">
+                <div class="blog-item" id="blog-item">
+                    <div class="blog-item__thumb">
+                        <a href="blog-details.html" class="blog-item__thumb-link">
+                            <img src="${item['image']}" alt="">
+                        </a>
+                    </div>
+                    <div class="blog-item__content">
+                        <ul class="text-list inline">
+                            <li class="text-list__item"> <span class="text-list__item-icon"><i
+                                        class="fas fa-calendar-alt"></i></span> 09 Jun 2023</li>
+                            <li class="text-list__item"> <span class="text-list__item-icon"><i
+                                        class="fa-solid fa-user"></i></span> By Admin</li>
+                        </ul>
+                        <h4 class="blog-item__title"><a href="blog-details.html" class="blog-item__title-link">${item['title']}</a></h4>
+                        <p class="blog-item__desc">${item['description']}</p>
+                        <a href="blog-details.html" class="btn--simple">Read More <span class="btn--simple__icon"><i
+                                    class="fas fa-arrow-right"></i></span></a>
+                    </div>
+                </div>
+            </div>`)
+
+            })
+
+
+        }catch(error) {
+            console.log(error);
+        }
+    })();
+     
+</script>
+{{-- @endsection --}}
 
 @endsection('conte')
