33319905f03ce11cbf7f3f8a1ff7c0bd4cf6482e
obaydullaa <cr7obaydul@gmail.com>
2023-07-17 07:04:23 +0600

    added users migration table.

---
+++ Module-20-laravel-step-19(16-july-23)/inventory/ProjectNote.php
@@ -0,0 +1,14 @@
+Phase 01 
+======================
+Developing User Auth Back-End Features
+1. User registration (end point)
+2. User login & JWT token (end point)
+3. Sending OTP Code to Email -Password Recover Stage 01 (end point)
+4. Verify OTP Code - Password Recover Stage 02 (end point)
+5. Allow user to reset password - Password Recover Stage 03 (end point)
+6. Allow user to get profile details (end point)
+7. Allow user ot update profile details (end point)
+
+/**
+* 1 [POS] Introduction
+*/
--- Module-20-laravel-step-19(16-july-23)/inventory/database/migrations/2023_07_17_004900_create_users.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/database/migrations/2023_07_17_004900_create_users.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('firstName', length: 50);
             $table->string('lastName', length: 50);
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 };
