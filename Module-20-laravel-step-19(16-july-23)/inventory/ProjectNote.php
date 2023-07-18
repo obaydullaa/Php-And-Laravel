ac4c7fa151b0169ea73ce9919a9aabc8431b2102
obaydullaa <cr7obaydul@gmail.com>
2023-07-18 08:24:49 +0600

    Added note file.

--- Module-20-laravel-step-19(16-july-23)/inventory/ProjectNote.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/ProjectNote.php
@@ -12,3 +12,63 @@ Developing User Auth Back-End Features
 /**
 * 1 [POS] Introduction
 */
+
+/**
+* 2 [POS] Registration
+*/
+
+/**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName', length: 50);
+            $table->string('lastName', length: 50);
+            $table->string('email', length: 50)->unique();
+            $table->string('mobile', length: 50);
+            $table->string('password', length: 50);
+            $table->string('otp', length: 50);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+
+    <!-- User Model  -->
+    class User extends Model
+{
+    protected $fillable = ['firstName', 'lastName', 'email', 'mobile', 'password', 'otp'];
+    protected $attributes = [
+        'otp' => '0'
+    ];
+}
+
+class UserController extends Controller
+{
+    function UserRegistration(Request $request) {
+
+        return User::create([
+            'firstName' => $request->input('firstName'),
+            'lastName' => $request->input('lastName'),
+            'email' => $request->input('email'),
+            'mobile' => $request->input('mobile'),
+            'password' => $request->input('password'),
+        ]);
+
+        
+    }
+}
+
+Route::post('/user-registration', [UserController::class,'UserRegistration']);
+
+// Off in kernel.php 
+// \App\Http\Middleware\VerifyCsrfToken::class, 
