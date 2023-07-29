9316420854e303dc1fd4324590fc1f04ef57e9ea
obaydullaa <cr7obaydul@gmail.com>
2023-07-29 10:23:35 +0600

    login rediect problem.

--- Module-20-laravel-step-19(16-july-23)/inventory/resources/views/components/auth/login-form.blade.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/resources/views/components/auth/login-form.blade.php
@@ -4,18 +4,18 @@
             <div class="card w-90  p-4">
                 <div class="card-body">
                     <h4>SIGN IN</h4>
-                    <br/>
-                    <input id="email" placeholder="User Email" class="form-control" type="email"/>
-                    <br/>
-                    <input id="password" placeholder="User Password" class="form-control" type="password"/>
-                    <br/>
+                    <br />
+                    <input id="email" placeholder="User Email" class="form-control" type="email" />
+                    <br />
+                    <input id="password" placeholder="User Password" class="form-control" type="password" />
+                    <br />
                     <button onclick="SubmitLogin()" class="btn w-100 btn-primary">Next</button>
-                    <hr/>
+                    <hr />
                     <div class="float-end mt-3">
                         <span>
-                            <a class="text-center ms-3 h6" href="{{url('/userRegistration')}}">Sign Up </a>
+                            <a class="text-center ms-3 h6" href="{{ url('/userRegistration') }}">Sign Up </a>
                             <span class="ms-1">|</span>
-                            <a class="text-center ms-3 h6" href="{{url('/sendOtp')}}">Forget Password</a>
+                            <a class="text-center ms-3 h6" href="{{ url('/sendOtp') }}">Forget Password</a>
                         </span>
                     </div>
                 </div>
@@ -24,30 +24,27 @@
     </div>
 </div>
 
-
 <script>
-
     async function SubmitLogin() {
-        let email=document.getElementById('email').value;
-        let password=document.getElementById('password').value;
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
 
-        if(email.length===0){
+        if (email.length === 0) {
             errorToast("Email is required");
-        }
-        else if(password.length===0){
+        } else if (password.length === 0) {
             errorToast("Password is required");
-        }
-        else{
+        } else {
             showLoader();
-            let res=await axios.post("/user-login",{email:email, password:password});
+            let res = await axios.post("/user-login", {
+                email: email,
+                password: password
+            });
             hideLoader()
-            if(res.status===200 && res.data['status']==='success'){
-                window.location.href="/dashboard";
-            }
-            else{
+            if (res.status === 200 && res.data['status'] === 'success') {
+                window.location.href = "/dashboard";
+            } else {
                 errorToast(res.data['message']);
             }
         }
     }
-
 </script>
