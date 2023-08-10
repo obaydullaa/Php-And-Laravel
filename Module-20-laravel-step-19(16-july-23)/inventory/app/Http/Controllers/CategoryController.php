e636fa991a949efb3f792580d94c007809f41062
obaydullaa <cr7obaydul@gmail.com>
2023-08-10 08:45:38 +0600

    Problem solve categoryController.

--- Module-20-laravel-step-19(16-july-23)/inventory/app/Http/Controllers/CategoryController.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/app/Http/Controllers/CategoryController.php
@@ -4,36 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 class CategoryController extends Controller
 {
     function CategoryPage() {
         return view('pages.dashboard.category-page');
     }
+
+    function CategoryList(Request $request) {
+        $user_id=$request->header('id');
+        return Category::where('user_id', $user_id)->get();
+        
+    }
+    function CategoryCreate(Request $request) {
+        $user_id=$request->header('id');
+        return Category::create([
+            'name' =>$request->input('name'),
+            'user_id' =>$user_id,
+        ]);
+    }
+    function CategoryDelete(Request $request) {
+        $category_id=$request->input('id');
+        $user_id=$request->header('id');
+        return Category::where('id', $category_id)->where('user_id', $user_id)->delete();
+    }
+    function CategoryUpdate(Request $request) {
+        $category_id=$request->input('id');
+        $user_id=$request->header('id');
+        return Category::where('id', $category_id)->where('user_id', $user_id)->update([
+            'name'=>$request->input('name'),
+        ]);
+    }
+
 }
 
-function CategoryList(Request $request) {
-    $user_id=$request->header('id');
-    return Category::where('user_id', $user_id)->get();
-    
-}
-function CategoryCreate(Request $request) {
-    $user_id=$request->header('id');
-    return Category::create([
-        'name' =>$request->input('name'),
-        'user_id' =>$user_id,
-    ]);
-}
-function CategoryDelete(Request $request) {
-    $category_id=$request->input('id');
-    $user_id=$request->header('id');
-    return Category::where('id', $category_id)->where('user_id', $user_id)->delete();
-}
-function CategoryUpdate(Request $request) {
-    $category_id=$request->input('id');
-    $user_id=$request->header('id');
-    return Category::where('id', $category_id)->where('user_id', $user_id)->update([
-        'name'=>$request->input('name'),
-    ]);
-}
+
