2cf6e1995fdfb3de3ac632585a70a1d717bbec78
obaydullaa <cr7obaydul@gmail.com>
2023-08-27 22:49:57 +0600

    cusercreate controller.

--- Module-20-laravel-step-19(16-july-23)/inventory/app/Http/Controllers/CustomerController.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    function CustomerPage():View{
+        return view('pages.dashboard.customer-page');
+    }
+
+
+    function CustomerCreate(Request $request){
+       $user_id = $request->header('id');
+        return Customer::create([
+            'name'=>$request->input('name'),
+            'email'=>$request->input('email'),
+            'mobile'=>$request->input('mobile'),
+            'user_id'=>$request->$user_id,
+        ]);
+    }
+
+
+
+
+
 }
