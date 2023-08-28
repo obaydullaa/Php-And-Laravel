4fc64c97e531a3cb124f513809744e91118769ab
obaydullaa <cr7obaydul@gmail.com>
2023-08-29 01:34:00 +0600

    added customer delete component.

--- Module-20-laravel-step-19(16-july-23)/inventory/resources/views/components/customer/customer-delete.blade.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/resources/views/components/customer/customer-delete.blade.php
@@ -16,20 +16,22 @@
         </div>
     </div>
 </div>
-
 <script>
-     async  function  itemDelete(){
-            let id=document.getElementById('deleteID').value;
-            document.getElementById('delete-modal-close').click();
-            showLoader();
-            let res=await axios.post("/delete-customer",{id:id})
-            hideLoader();
-            if(res.data===1){
-                successToast("Request completed")
-                await getList();
-            }
-            else{
-                errorToast("Request fail!")
-            }
-     }
+    async  function  itemDelete(){
+        let id=document.getElementById('deleteID').value;
+        document.getElementById('delete-modal-close').click();
+        showLoader();
+        let res=await axios.post("/delete-customer",{id:id})
+        hideLoader();
+        if(res.data===1){
+            successToast("Request completed")
+            await getList();
+        }
+        else{
+            errorToast("Request fail!")
+        }
+    }
 </script>
+
+
+
