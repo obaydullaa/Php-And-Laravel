a2398ce8ffa826fa7e32997fed73a967d5b3abc1
obaydullaa <cr7obaydul@gmail.com>
2023-09-07 09:30:56 +0600

    Added note file code .

--- Module-20-laravel-step-19(16-july-23)/inventory/ProjectNote.php
+++ Module-20-laravel-step-19(16-july-23)/inventory/ProjectNote.php
@@ -1276,3 +1276,4 @@ view->pages->dashboard->custoper-ages.blade.php
  *  31 [POS] Update Category By Getting Details
  =================================================================
  */
+category-update.blade.php ->
